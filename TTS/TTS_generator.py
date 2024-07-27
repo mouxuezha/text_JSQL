69cc5921546c3a41c03752199a04c83044333673
mouxuezha <424180922@qq.com>
2024-07-27 14:06:18 +0800

    算是完全调通了，从文字到合成音频到念出来。

--- TTS/TTS_generator.py
+++ TTS/TTS_generator.py
@@ -7,20 +7,23 @@ import _thread as thread
 import base64
 import hmac
 import json
-import datetime
+from datetime import datetime
 from wsgiref.handlers import format_date_time
 from time import mktime
 from urllib.parse import urlencode
 import websocket
 import os
 from pydub import AudioSegment
+import ssl
 
 class TTS_generator():
 
     def __init__(self) -> None:
       
         self.log_model_communication_name = r"TTS\log.txt"
-        self.auto_text_location = r"auto_test"
+        self.auto_test_location = r"C:\Users\42418\Desktop\2024ldjs\EnglishMulu\text_JSQL\auto_test"
+        # self.auto_test_location = r"auto_test"
+        self.ffmpeg_location = r"E:\ruanjian\ffmpeg-7.0.1-essentials_build\ffmpeg-7.0.1-essentials_build\bin\ffmpeg.exe"
         self.__init_AKSK()
         self.__init_net()
         pygame.init()
@@ -41,7 +44,7 @@ class TTS_generator():
 
     def __init_net(self):
         self.url = self.create_url()
-        self.ws = websocket.WebSocketApp(self.url, on_message=self.text_to_voice, on_error=self.on_error, on_close=self.on_close)
+        self.ws = websocket.WebSocketApp(self.url, on_message=self.on_message, on_error=self.on_error, on_close=self.on_close)
 
     def create_url(self):
         url = 'wss://tts-api.xfyun.cn/v2/tts'
@@ -76,7 +79,7 @@ class TTS_generator():
         return url
 
     
-    def on_message(self,text:str):
+    def on_message(self,ws,message):
         try:
             message =json.loads(message)
             code = message["code"]
@@ -87,15 +90,15 @@ class TTS_generator():
             print(message)
             if status == 2:
                 print("ws is closed")
-                self.ws.close()
+                ws.close()
             if code != 0:
                 errMsg = message["message"]
                 print("sid:%s call error:%s code is:%s" % (sid, errMsg, code))
             else:
 
-                with open(self.auto_text_location+r'\demo.pcm', 'ab') as f:
+                with open(self.auto_test_location+r'\demo1.pcm', 'ab') as f:
                     f.write(audio)
-                # with open(self.auto_text_location+r'\demo.mp3', 'ab') as f:
+                # with open(self.auto_test_location+r'\demo.mp3', 'ab') as f:
                 #     f.write(audio)
 
         except Exception as e:
@@ -106,18 +109,46 @@ class TTS_generator():
         self.Text = text
         self.Data = {"status": 2, "text": str(base64.b64encode(self.Text.encode('utf-8')), "UTF8")}
 
-    def display_voice(self,location):
+        # 然后开始通信了
+        tonfxin_thread = threading.Thread(target=self.run_single)
+        tonfxin_thread.start()
+    
+    def run_single(self):
+        # pass
+        self.ws.on_open = self.on_open
+        self.ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+        pass 
+
+    def display_voice(self,location,name):
         # 简单的放一段，看看成色。
         # 这个东西按理说应该实现成一个异步的，让它自己跑着。来个线程池啥的？
         # 算了先别整list了，一个一个来好了。开一次这个函数就是播一段
-        display_thread = threading.Thread(target=self.display_voice_single,args=(location,))
+        pcm_location = location + "\\" + name
+        mp3_location = self.pcm_to_mp3(location,name[0:-4]) 
+        display_thread = threading.Thread(target=self.display_voice_single,args=(mp3_location,))
         display_thread.start()
         pass
+    
+    def pcm_to_mp3(self,location,name):
+        pcm_location = location + "\\" + name + ".pcm"
+        mp3_location = location + "\\" + name + ".mp3"
+        sample_width = 2
+        frame_rate = 16000
+        channels = 1
+        audio = AudioSegment.from_file(pcm_location, format="pcm", parameters=["-ar", "44100", "-f", "s16le"],sample_width=sample_width,frame_rate=frame_rate,channels=channels)
+        audio.converter = self.ffmpeg_location # 暴力手改不可取，但是它既然是写死的，没考虑Windows下，那也就怪不得我了
+        audio.export(mp3_location, format="mp3")
+        return mp3_location
 
     def display_voice_single(self,location:str):
         # 单独的放一段，看看成色。
         pygame.mixer.music.load(location)
-        pygame.mixer.music.play()
+        pygame.mixer.music.play(1)
+        # 等播放结束
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+
+        pygame.mixer.music.stop()
         pass
 
     def load_txt(self,file_name):
@@ -154,12 +185,13 @@ class TTS_generator():
             d = json.dumps(d)
             print("------>开始发送文本数据")
             ws.send(d)
-            if os.path.exists(self.auto_text_location+r'\demo.pcm'):
-                os.remove(self.auto_text_location+r'\demo.pcm')
+            if os.path.exists(self.auto_test_location+r'\demo.pcm'):
+                os.remove(self.auto_test_location+r'\demo.pcm')
 
         thread.start_new_thread(run, ())
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     TTS = TTS_generator()
-    TTS.text_to_voice("当朝大学士，统共有五位。")
-    TTS.display_voice(TTS.auto_text_location+r'\demo.pcm')
+    # TTS.text_to_voice("当朝大学士，统共有五位，朕不得不罢免四位")
+    TTS.display_voice(TTS.auto_test_location, r'demo1.pcm')
+    # TTS.display_voice_single(r'C:\\Users\\42418\\Desktop\\2024ldjs\\EnglishMulu\\text_JSQL\\auto_test\\demo1.mp3')
