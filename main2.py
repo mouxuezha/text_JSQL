019dd84d587b94914e61a33ddf1d5c6c7a511f63
mouxuezha <424180922@qq.com>
2025-02-19 10:22:32 +0800

    增加了单机QtC++人混的支持，但是还没来得及调试。下午再调试也不是不行，但是得尽早搞

---
+++ main2.py
@@ -0,0 +1,56 @@
+# 这个是整理一下之前的，实现单台电脑上的、使用雪楠哥他们前端的，人机交互
+
+from dialog_box.dialog_box_server import MyWidget as MyWidget_server
+from dialog_box.dialog_box_client2 import MyWidget as MyWidget_client
+from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtWidgets import QDialog 
+
+class auto_run_comunicator():
+    # 本来是早都该弄的了，自动化的启动器。那现在弄一下好了。
+    def __init__(self):
+        # 初始化一系列的通信啥的东西。
+        self.config = {"red_ip":"192.168.1.114", "red_port": "20001",
+                    "blue_ip": "192.168.1.114", "blue_port": "20002","flag_server_waiting":True,"dialog_box_model": "QtC++","socket_debug_model":"net_debug"}
+        self.__init_server()
+        self.__init_client_red()
+        self.__init_client_blue()
+
+        # 原则上初始化完了就可以看着了，只要保证主线程不退出。
+    
+    def __init_server(self):
+        app = QtWidgets.QApplication([])
+        config = self.config
+        widget = MyWidget_server(config=config)
+        widget.resize(800, 300)
+        widget.show()        
+        pass 
+
+    def __init_client_red(self):
+        config = self.config    # dialog_box_model能选QtC++和QtPython，socket_debug_model有local_debug和net_debug，区别是用不用那个假的socket类。
+        # 这个config其实是服务于command_processor的，不是服务于dialogbox，所以原则上其他的dialogbox也可以用。
+        widget = MyWidget_client(role="red_player",config = config)
+        widget.run_mul()        
+        pass 
+    
+    def __init_client_blue(self):
+        config = self.config    # dialog_box_model能选QtC++和QtPython，socket_debug_model有local_debug和net_debug，区别是用不用那个假的socket类。
+        # 这个config其实是服务于command_processor的，不是服务于dialogbox，所以原则上其他的dialogbox也可以用。
+        widget = MyWidget_client(role="blue_player",config = config)
+        widget.run_mul()        
+        pass 
+
+    def run_mul(self):
+        # 这个就是开一个主线程等着而已。既然如此那做一个命令行退出的东西可也。
+        while(True):
+            user_input = input("输入exit退出程序。")
+            if user_input == "exit":
+                break
+            else:
+                # 实际效果是输入错了就重新输。也留出后续扩展其他命令行功能的接口。
+                pass
+
+        pass
+
+if __name__ == "__main__":
+    shishi = auto_run_comunicator()
+    shishi.run_mul()
